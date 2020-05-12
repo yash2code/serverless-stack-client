9e141d6ff609eef6f5eccc985e6f8b650c4ab637
yash chaudhary <yash240196@gmail.com>
2020-05-13 00:00:01 +0530

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'ap-south-1',
+    BUCKET: 'weadmit-bucket',
+  },
+  apiGateway: {
+    REGION: 'YOUR_API_GATEWAY_REGION',
+    URL: 'YOUR_API_GATEWAY_URL',
+  },
+  cognito: {
+    REGION: 'ap-south-1',
+    USER_POOL_ID: 'ap-south-1_0TmZjaiN2',
+    APP_CLIENT_ID: '2lkbt2qsja8j9semhpvns01n3p',
+    IDENTITY_POOL_ID: 'ap-south-1:bfb1d449-8033-4d63-af5c-136d2c4a0e92',
+  },
+}
