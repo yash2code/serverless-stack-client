822a1550f3bc87028545c1572d72dd142990655c
yash chaudhary <yash240196@gmail.com>
2020-05-19 10:40:12 +0530

    convert audio to mp3 using ffmpeg for transcription purpose

--- server/index.js
+++ server/index.js
@@ -5,17 +5,38 @@ const pino = require('express-pino-logger')()
 const { chatToken, videoToken, voiceToken } = require('./tokens')
 const Twilio = require('twilio')
 const request = require('request-promise')
-// var ffmpeg = require('fluent-ffmpeg')
+var ffmpeg = require('fluent-ffmpeg')
 const app = express()
 const AWS = require('aws-sdk')
-// const fs = require('fs')
+const fs = require('fs')
+const streams = require('memory-streams')
+const toWav = require('audiobuffer-to-wav')
+const AudioContext = require('web-audio-api').AudioContext
+const audioContext = new AudioContext()
+const axios = require('axios')
+
+const download_media = (url, media_path) =>
+  axios({
+    url,
+    responseType: 'stream',
+  }).then(
+    (response) =>
+      new Promise((resolve, reject) => {
+        response.data
+          .pipe(fs.createWriteStream(media_path))
+          .on('finish', () => resolve())
+          .on('error', (e) => reject(e))
+      })
+  )
 
 AWS.config.update({
   accessKeyId: config.aws.accessKeyId,
   secretAccessKey: config.aws.secretAccessKey,
+  region: 'ap-south-1',
 })
 
 var s3 = new AWS.S3()
+var transcribeservice = new AWS.TranscribeService({ region: 'ap-south-1' })
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -36,24 +57,103 @@ const load = async (options, recordingSid, container) => {
       Body: body,
     })
     .promise()
+
+  console.log(uploadResult)
+  if (container == 'mka') {
+    var params = {
+      LanguageCode: 'en-US',
+      Media: {
+        MediaFileUri: uploadResult.Location,
+      },
+      OutputBucketName: 'weadmit-transcribe-bucket',
+      TranscriptionJobName: `${recordingSid}`,
+    }
+    transcribeservice.startTranscriptionJob(params, function (err, data) {
+      if (err) console.log(err, err.stack)
+      // an error occurred
+      else console.log(data) // successful response
+    })
+  }
+
+  return uploadResult
+}
+
+const getBuffer = async (options) => {
+  const body = await request(options)
+  return body
 }
 app.post('/api/statuscallback', (req, res) => {
   res.setHeader('Content-Type', 'application/x-www-urlencoded')
   console.log(req.body.RecordingSid, 'req in callback', req.body)
   const statusCallbackEvent = req.body.StatusCallbackEvent
+  console.log(req.body)
   if (statusCallbackEvent == 'recording-completed') {
     const container = req.body.Container
     const recordingSid = req.body.RecordingSid
     const uri = `https://video.twilio.com/v1/Recordings/${recordingSid}/Media`
-    client.request({ method: 'GET', uri: uri }).then((response) => {
+    client.request({ method: 'GET', uri: uri }).then(async (response) => {
       const mediaLocation = response.body.redirect_to
       const options = {
         uri: mediaLocation,
         encoding: null,
       }
-      // media.push(mediaLocation)
-      load(options, recordingSid, container)
 
+      await download_media(mediaLocation, `${recordingSid}.${container}`)
+
+      media.push(`${recordingSid}.${container}`)
+      console.log(media, 'media')
+      if (container == 'mka') {
+        // let resp = await getBuffer(options)
+        // let resp = fs.readFileSync(mediaLocation);
+        // console.log(resp)
+        // audioContext.decodeAudioData(`${mediaLocation}.mka`, buffer => {
+        //   let wav = toWav(buffer);
+        //   console.log(wav,' in wav')
+        //   // do something with the WAV ArrayBuffer ...
+        // });
+        await new ffmpeg(fs.createReadStream(`${recordingSid}.mka`))
+          .toFormat('mp3')
+          // .output(`${recordingSid}.mp3`)
+          .on('progress', function (progress) {
+            console.log('Processing: ' + progress.percent + '% done')
+          })
+          .on('end', function () {
+            console.log('Processing finished !')
+            var params = {
+              Body: fs.createReadStream(`${recordingSid}.mp3`),
+              Bucket: config.aws.bucketName,
+              Key: `${recordingSid}.mp3`,
+            }
+            s3.upload(params, function (err, data) {
+              //handle error
+              if (err) {
+                console.log('Error', err)
+              }
+
+              //success
+              if (data) {
+                console.log('Uploaded in:', data.Location)
+                var params = {
+                  LanguageCode: 'en-US',
+                  Media: {
+                    MediaFileUri: data.Location,
+                  },
+                  OutputBucketName: 'weadmit-transcribe-bucket',
+                  TranscriptionJobName: `${recordingSid}`,
+                }
+                transcribeservice.startTranscriptionJob(params, function (
+                  err,
+                  data
+                ) {
+                  if (err) console.log(err, err.stack)
+                  // an error occurred
+                  else console.log(data) // successful response
+                })
+              }
+            })
+          })
+          .save(`${recordingSid}.mp3`)
+      }
       // if (media.length > 1) {
 
       //    ffmpeg(media[0])
